e5211ee99a0f7b3b2226ae83d2591bb8e5bfbe94
Yulia Balenko <yulia@YuliaBookPro13.local>
2022-11-28 22:22:28 -0800

    added 5 exercises of Class 6 Homework

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,20 @@
 // Do not change
 document.getElementById('cat').addEventListener('click', () => {
-  alert('meow!');
+    alert('meow!');
 });
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+document.querySelector("#more-info").addEventListener('click', (e) => {
+    e.target.removeAttribute('href');
+    alert("Here's some info");
+});
+
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+document.getElementById('dog').addEventListener('click', (e) => {
+    e.stopPropagation();
+    alert("Bow wow!");
+});
